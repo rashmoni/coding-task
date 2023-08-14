0a645452a603923fd579d14dacdb56559d21c0a5
rashmoni <rashudey9851@gmail.com>
2023-08-14 09:59:18 +0530

    Implemented 3 methods and respective unit tests

--- src/main/java/com/minnity/report/ReportService.java
+++ src/main/java/com/minnity/report/ReportService.java
@@ -1,7 +1,7 @@
 package com.minnity.report;
 
-import org.apache.commons.lang3.NotImplementedException;
-
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,16 +9,57 @@ public class ReportService {
 
   //task 1: Return number of requests that were made for each company. (e.g. companyId -> requestNumber)
   public Map<Integer, Long> calculateNumberOfRequestsPerCompany(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+
+    Map<Integer, Long> numberOfRequestsPerCompany = new HashMap<>();
+
+    for (RequestLog log : requestLogs) {
+      int companyId = log.getCompanyId();
+      numberOfRequestsPerCompany.put(companyId, numberOfRequestsPerCompany.getOrDefault(companyId, 0L) + 1);
+    }
+
+    return numberOfRequestsPerCompany;
   }
 
   //task 2: Count and return requests per company that finished with an error HTTP response code (>=400)
   public Map<Integer, RequestLog> findRequestsWithError(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    Map<Integer, RequestLog> requestsWithError = new HashMap<>();
+
+    for (RequestLog log : requestLogs) {
+      if (log.getRequestStatus() >= 400) {
+        requestsWithError.put(log.getCompanyId(), log);
+      }
+    }
+    return requestsWithError;
   }
 
   //task 3: find and print API (requests path) that on average takes the longest time to process the request.
   public String findRequestPathWithLongestDurationTime(List<RequestLog> requestLogs) {
-    throw new NotImplementedException("TODO implement this method");
+    Map<String, List<Long>> pathToDurations = new HashMap<>();
+
+    for (RequestLog log : requestLogs) {
+      String requestPath = log.getRequestPath();
+      long durationTime = log.getRequestDuration();
+
+      if (!pathToDurations.containsKey(requestPath)) {
+        pathToDurations.put(requestPath, new ArrayList<>());
+      }
+
+      pathToDurations.get(requestPath).add(durationTime);
+    }
+
+    String longestDurationPath = null;
+    double longestAvgDuration = 0.0;
+
+    for (Map.Entry<String, List<Long>> entry : pathToDurations.entrySet()) {
+      List<Long> durations = entry.getValue();
+      double avgDuration = durations.stream().mapToLong(Long::valueOf).average().orElse(0.0);
+
+      if (avgDuration > longestAvgDuration) {
+        longestAvgDuration = avgDuration;
+        longestDurationPath = entry.getKey();
+      }
+    }
+
+    return longestDurationPath;
   }
 }
--- src/test/java/com/minnity/report/ReportServiceTest.java
+++ src/test/java/com/minnity/report/ReportServiceTest.java
@@ -2,11 +2,105 @@ package com.minnity.report;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 
 public class ReportServiceTest {
 
   @Test
-  public void todo() {
-    RequestLog sampleRequest = SampleDataGenerator.aRequestLog();
+  public void testCalculateNumberOfRequestsPerCompany() {
+    RequestLog log1 = mock(RequestLog.class);
+    when(log1.getCompanyId()).thenReturn(1);
+
+    RequestLog log2 = mock(RequestLog.class);
+    when(log2.getCompanyId()).thenReturn(2);
+
+    RequestLog log3 = mock(RequestLog.class);
+    when(log3.getCompanyId()).thenReturn(1);
+
+    RequestLog log4 = mock(RequestLog.class);
+    when(log4.getCompanyId()).thenReturn(3);
+
+    List<RequestLog> requestLogs = new ArrayList<>();
+    requestLogs.add(log1);
+    requestLogs.add(log2);
+    requestLogs.add(log3);
+    requestLogs.add(log4);
+
+    ReportService reportService = new ReportService();
+    Map<Integer, Long> result = reportService.calculateNumberOfRequestsPerCompany(requestLogs);
+
+    assertEquals((Long) 2L, result.get(1));
+    assertEquals((Long) 1L, result.get(2));
+    assertEquals((Long) 1L, result.get(3));
+  }
+
+  @Test
+  public void testFindRequestsWithError() {
+    RequestLog log1 = mock(RequestLog.class);
+    when(log1.getCompanyId()).thenReturn(1);
+    when(log1.getRequestStatus()).thenReturn(200);
+
+    RequestLog log2 = mock(RequestLog.class);
+    when(log2.getCompanyId()).thenReturn(2);
+    when(log2.getRequestStatus()).thenReturn(500);
+
+    RequestLog log3 = mock(RequestLog.class);
+    when(log3.getCompanyId()).thenReturn(1);
+    when(log3.getRequestStatus()).thenReturn(400);
+
+    RequestLog log4 = mock(RequestLog.class);
+    when(log4.getCompanyId()).thenReturn(3);
+    when(log4.getRequestStatus()).thenReturn(403);
+
+    List<RequestLog> requestLogs = new ArrayList<>();
+    requestLogs.add(log1);
+    requestLogs.add(log2);
+    requestLogs.add(log3);
+    requestLogs.add(log4);
+
+    ReportService reportService = new ReportService();
+    Map<Integer, RequestLog> result = reportService.findRequestsWithError(requestLogs);
+
+    assertEquals(3, result.size());
+    assertEquals(log2, result.get(2));
+    assertEquals(log3, result.get(1));
+
+  }
+
+  @Test
+  public void testFindRequestPathWithLongestDurationTime() {
+    RequestLog log1 = mock(RequestLog.class);
+    when(log1.getRequestPath()).thenReturn("/api/user/orders");
+    when(log1.getRequestDuration()).thenReturn(1000L);
+
+    RequestLog log2 = mock(RequestLog.class);
+    when(log2.getRequestPath()).thenReturn("/api/user/details");
+    when(log2.getRequestDuration()).thenReturn(1000L);
+
+    RequestLog log3 = mock(RequestLog.class);
+    when(log3.getRequestPath()).thenReturn("/api/user/orders");
+    when(log3.getRequestDuration()).thenReturn(1500L);
+
+    RequestLog log4 = mock(RequestLog.class);
+    when(log4.getRequestPath()).thenReturn("/api/user/id");
+    when(log4.getRequestDuration()).thenReturn(800L);
+
+    List<RequestLog> requestLogs = new ArrayList<>();
+    requestLogs.add(log1);
+    requestLogs.add(log2);
+    requestLogs.add(log3);
+    requestLogs.add(log4);
+
+    ReportService reportService = new ReportService();
+    String result = reportService.findRequestPathWithLongestDurationTime(requestLogs);
+
+    assertEquals("/api/user/orders", result);
+
   }
 }
